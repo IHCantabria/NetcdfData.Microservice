bc119fd04989d6f09b80571ae14fd5950d01bf65
Marco Vega <vegama@unican.es>
2023-01-16 17:38:16 +0100

    Black

--- app/api/netcdfs.py
+++ app/api/netcdfs.py
@@ -7,9 +7,16 @@ import json
 env = json.load(open(".env"))
 router = APIRouter()
 
+
 @router.get("/")
-def get_netcdf_from_point(longitude: float, latitude: float, filepath: str, start_date: str, end_date: str):
+def get_netcdf_from_point(
+    longitude: float, latitude: float, filepath: str, start_date: str, end_date: str
+):
     ds = get_netcdf(longitude, latitude, filepath, start_date, end_date)
     unique_filename = str(uuid.uuid4())
-    ds.to_netcdf("{0}/{1}.nc".format(env["tmp_folder"],unique_filename))
-    return FileResponse("{0}/{1}.nc".format(env["tmp_folder"],unique_filename), media_type="application/x-netcdf", filename="data.nc")
+    ds.to_netcdf("{0}/{1}.nc".format(env["tmp_folder"], unique_filename))
+    return FileResponse(
+        "{0}/{1}.nc".format(env["tmp_folder"], unique_filename),
+        media_type="application/x-netcdf",
+        filename="data.nc",
+    )
