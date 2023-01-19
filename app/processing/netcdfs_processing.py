29a8a736160458d2d14feeda22e81a10dcc6decc
Marco Vega <vegama@unican.es>
2023-01-19 11:15:45 +0100

    Updated processing docstream

--- app/processing/netcdfs_processing.py
+++ app/processing/netcdfs_processing.py
@@ -2,6 +2,18 @@ import os
 import xarray as xr
 
 def get_netcdf_from_point(longitude: float, latitude: float, filepath: str, start_date: str, end_date: str):
+    """_summary_
+
+    Args:
+        longitude (float): Longitude of the point
+        latitude (float): Latitude of the point
+        filepath (str): Filepath of the netcdf file
+        start_date (str): Start date of the data
+        end_date (str): End date of the data
+
+    Returns:
+        _type_: xarray dataset
+    """
     files = os.listdir(filepath)
     if len(files) >1:
         ds = xr.open_mfdataset("{0}/*.nc".format(filepath), concat_dim="time", combine='nested', engine="netcdf4")
