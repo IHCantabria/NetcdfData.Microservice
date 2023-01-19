0a3d9c191a7deed97ad4ede503d1bfc879685918
Marco Vega <vegama@unican.es>
2023-01-19 10:56:52 +0100

    Generated docs

--- app/api/netcdfs.py
+++ app/api/netcdfs.py
@@ -12,6 +12,18 @@ router = APIRouter()
 def get_netcdf_from_point(
     longitude: float, latitude: float, filepath: str, start_date: str, end_date: str
 ):
+    """_summary_
+
+    Args:
+        longitude (float): _description_
+        latitude (float): _description_
+        filepath (str): _description_
+        start_date (str): _description_
+        end_date (str): _description_
+
+    Returns:
+        _type_: _description_
+    """
     ds = get_netcdf(longitude, latitude, filepath, start_date, end_date)
     unique_filename = str(uuid.uuid4())
     ds.to_netcdf("{0}/{1}.nc".format(env["tmp_folder"], unique_filename))
