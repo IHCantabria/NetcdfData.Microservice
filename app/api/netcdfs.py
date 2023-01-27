52223223500820464b5dd5f9ad323759bbc3d3ad
root <root@IHPC8.ihcantabria.unican.es>
2023-01-27 11:15:40 +0100

    Remove envs

--- app/api/netcdfs.py
+++ app/api/netcdfs.py
@@ -4,7 +4,6 @@ import uuid
 from app.processing.netcdfs_processing import get_netcdf_from_point as get_netcdf
 import json
 
-env = json.load(open(".env"))
 router = APIRouter()
 
 
@@ -26,9 +25,9 @@ def get_netcdf_from_point(
     """
     ds = get_netcdf(longitude, latitude, filepath, start_date, end_date)
     unique_filename = str(uuid.uuid4())
-    ds.to_netcdf("{0}/{1}.nc".format(env["tmp_folder"], unique_filename))
+    ds.to_netcdf("/tmp/{0}.nc".format(unique_filename))
     return FileResponse(
-        "{0}/{1}.nc".format(env["tmp_folder"], unique_filename),
+        "/tmp/{0}.nc".format(unique_filename),
         media_type="application/x-netcdf",
         filename="data.nc",
     )
